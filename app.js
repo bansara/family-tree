00d9c651089bf5ebed18ef25c45cf50e91f55676
Josh Geisler <joshuageisler@gmail.com>
2020-04-27 08:14:41 -0400

    refactored DRYer

--- app.js
+++ app.js
@@ -13,68 +13,48 @@ button.addEventListener('click', getItOn);
 
 function getItOn() {
     if (!name.value) return;
+
     const person = whosYourDaddy(name.value);
     let newDivs = document.querySelectorAll('.new');
     let numNewDivs = newDivs.length;
 
     if (numNewDivs === 0) {
-        const child = newPerson(person.value);
-        const div = document.createElement('div');
-        div.appendChild(child);
-        div.classList.add('new', 'active');
-        child.classList.add('activePerson');
-        family.appendChild(div);
+        addDivAndPerson(person.value)
         parent.innerText = person.value;
     }
     if (numNewDivs === 1) {
-        const child = newPerson(person.value);
-        const div = document.createElement('div');
-        div.appendChild(child);
-        div.classList.add('new');
-        family.appendChild(div);
+        addDivAndPerson(person.value)
     }
     if (numNewDivs > 1) {
         const arrayOfNewDivs = [...newDivs]; // so I can use array methods on a node list
-        const divToAppend = arrayOfNewDivs.findIndex(div => {
-            return div.querySelectorAll('.activePerson').length > 0 ? true : false;
-        })
+        const divToAppend = arrayOfNewDivs.findIndex(div => div.querySelectorAll('.activePerson').length)
         if (divToAppend === numNewDivs - 1) {
-            const child = newPerson(person.value);
-            const div = document.createElement('div');
-            div.appendChild(child);
-            div.classList.add('new');
-            family.appendChild(div);
+            addDivAndPerson(person.value)
         } else {
             const child = newPerson(person.value);
             arrayOfNewDivs[divToAppend + 1].appendChild(child);
         }
     }
 
-    
-    
-    const children = document.querySelectorAll('.person');
-    children.forEach(div => {
-        div.removeEventListener('click', activatePerson);
-        div.addEventListener('click', activatePerson);
-    })
-    
     familyList.push(person);
-    if(familyList.length){
+
+    if (familyList.length === 1) {
         log.classList.remove('invisible');
         title.innerText = `${familyList[0].value}'s Family Tree`
-        tree.innerText = `${familyList[0].log()}`;
     }
-    console.log(familyList);
+
+    tree.innerText = `${familyList[0].log()}`;
+
     name.focus();
 
 }
-
 
 function activatePerson() {
     const active = document.querySelectorAll('.activePerson');
     active.forEach(element => element.classList.remove('activePerson'))
     this.classList.add('activePerson');
     parent.innerText = this.childNodes[0].innerText;
+    name.focus();
 }
 
 function newPerson(name) {
@@ -83,8 +63,18 @@ function newPerson(name) {
     h2.innerText = name;
     div.appendChild(h2);
     div.classList.add('person');
-    div.setAttribute('data-familylist', `${familyList.length}`)
+    div.setAttribute('data-familylist', `${familyList.length}`);
+    div.addEventListener('click', activatePerson);
     return div;
+}
+
+function addDivAndPerson(name) {
+    const child = newPerson(name);
+    const div = document.createElement('div');
+    div.appendChild(child);
+    div.classList.add('new');
+    family.appendChild(div);
+    if (!familyList.length) child.classList.add('activePerson');
 }
 
 function whosYourDaddy(name) {
@@ -96,7 +86,7 @@ function whosYourDaddy(name) {
         const index = daddy.dataset.familylist;
         familyList[index].insert(name);
         const length = familyList[index].children.length;
-        
+
         return familyList[index].children[length - 1];
     }
 }
